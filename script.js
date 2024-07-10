ddb0a3bc906fb88b75a1990519ca0451d4c251e4
Tejdeep Nookala <iamtejdeepn@gmail.com>
2024-07-10 23:45:02 +0530

    Fully done the Setup the Initial Boxes and BoxSizes into variables

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const boxGrid = document.querySelector("#boxes")
 const body = document.querySelector("body")
+const input = document.querySelector("#input")
+const submit = document.querySelector("#submit")
+
+let initialBoxSize = '3.125rem'
+let initialBoxes=256
 
 
-function createDiv(){
+function createDiv(boxSize){
     let div = document.createElement("div")
     div.className = "box"
     
-    div.style.height = '3.125rem'
-    div.style.width = '3.125rem'
+    div.style.height = boxSize
+    div.style.width = boxSize
     // div.style.backgroundColor = '#84c7e3'
     // div.style.border= '1px solid black'
 
@@ -15,18 +20,18 @@ function createDiv(){
 
 }
 
-function createGrid(){
+function createGrid(totalBoxes,boxSize){
     let boxArray = []
-    for(let i = 0;i<256;i++){
+    for(let i = 0;i<totalBoxes;i++){
 
-        boxArray.push(createDiv())
+        boxArray.push(createDiv(boxSize))
         boxGrid.appendChild(boxArray[i])
 
     }
 
 }
 
-createGrid()
+createGrid(initialBoxes,initialBoxSize)
 
 const allBox = document.querySelectorAll(".box")
 
@@ -36,6 +41,33 @@ allBox.forEach((box)=>{
     })
 })
 
+
+function DeleteAllBoxes(){
+    const allBox = document.querySelectorAll(".box");
+    allBox.forEach((box)=>{
+        boxGrid.removeChild(box)
+    })
+    
+}
+
+submit.addEventListener('click',()=>{
+
+    //add solution for CornerCase: if input is 0
+    if(input.value>100 || input.value<0){
+        alert("Please enter a positive value below 100")
+        return
+    }
+
+    DeleteAllBoxes()
+
+    noOfBoxes = input.value
+    let size = 50/noOfBoxes
+    
+    createGrid(noOfBoxes)
+    
+
+
+})
 
 
 
